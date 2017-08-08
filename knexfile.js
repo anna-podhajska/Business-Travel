ea0ad4a96507629828948600710c549e75950113
Anna P <podhajskaa@gmail.com>
2017-08-08 18:07:15 +1200

    fix connection for production

--- knexfile.js
+++ knexfile.js
@@ -24,11 +24,7 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    connection: process.env.DATABASE_URl,
     pool: {
       min: 2,
       max: 10
